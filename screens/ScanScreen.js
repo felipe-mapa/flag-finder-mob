78f4abbe55a8fdd5a8300fd8f9e6af1aadea2def
Felipe <felipe@myvirtualmission.com>
2022-03-24 20:02:12 +1300

    Deleted unused packages

--- screens/ScanScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {UnityView} from 'react-native-unity-view';
-
-const ScanScreen = () => {
-  return <UnityView style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, }} />
-
-}
-
-export default ScanScreen
